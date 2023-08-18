537f09f8a1eaac74ebcd2dbe8c286122bc6b901e
Kyle Chadworth <108600865+kylechadworth@users.noreply.github.com>
2023-08-18 16:53:44 -0400

    Created main.cpp
    Added source file

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    HWND hWnd = FindWindowA(NULL, "Ubisoft Connect");
+    if (hWnd == NULL) {
+        std::cout << "Ubisoft aint running." << std::endl;
+        return -1;
+    }
+    DWORD pid;
+    GetWindowThreadProcessId(hWnd, &pid);
+    HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+    if (hProc == NULL) {
+        std::cout << "Cant access the ubi" << std::endl;
+        return -1;
+    }
+    TerminateProcess(hProc, 0);
+    MessageBoxA(NULL, "Ubi gone.", "Done!", MB_OK);
+    return 0;
+}
